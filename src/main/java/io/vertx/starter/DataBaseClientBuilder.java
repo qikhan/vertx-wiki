f54f4c9e740a601d5c4642739b6e13d8cd0aaedc
Quamrul Khan <quamrul.khan@gmail.com>
2017-07-14 00:21:47 -0400

    firt commit

---
+++ src/main/java/io/vertx/starter/DataBaseClientBuilder.java
@@ -0,0 +1,63 @@
+package io.vertx.starter;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+
+
+/**
+ * Created by qikhan on 7/13/2017.
+ */
+public class DataBaseClientBuilder {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DataBaseClientBuilder.class);
+
+  private static final String SQL_CREATE_PAGES_TABLE = "create table " +
+    " if not exists Pages " +
+    " ( " +
+    "  Id integer identity primary key " +
+    ", Name varchar(255) unique " +
+    ", Content clob " +
+    ")";
+
+  private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+  private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+  private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+  private static final String SQL_ALL_PAGES = "select Name from Pages";
+  private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+
+  public static Future<JDBCClient> build(Vertx vertx) {
+
+    Future<JDBCClient> future = Future.future();
+
+    JDBCClient jdbcClient = JDBCClient.createShared(vertx, new JsonObject()
+      .put("url", "jdbc:hsqldb:file:db/wiki")
+      .put("driver_class", "org.hsqldb.jdbcDriver")
+      .put("max_pool_size", 30));
+
+    jdbcClient.getConnection(ar -> {
+      if (ar.failed()) {
+        LOGGER.error("Could not open a database connection", ar.cause());
+        future.fail(ar.cause());
+      } else {
+        SQLConnection connection = ar.result();
+        connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+          connection.close();
+          if (create.failed()) {
+            LOGGER.error("Database preparation error", create.cause());
+            future.fail(create.cause());
+          }
+          {
+            future.complete(jdbcClient);
+          }
+        });
+      }
+    });
+
+    return future;
+  }
+}
---
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,107 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+public class MainVerticle extends AbstractVerticle {
+
+  private JDBCClient dbClient;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+  @Override
+  public void start(Future<Void> startFuture) {
+    Future<Void> steps = initDatabase().compose(v -> initHttpServer());
+    steps.setHandler(startFuture.completer());
+  }
+
+  private Future<Void> initDatabase() {
+    Future<Void> future = Future.future();
+
+    Future<JDBCClient> dbClientFuture = DataBaseClientBuilder.build(vertx);
+    if (dbClientFuture.failed()) {
+      future.fail(dbClientFuture.cause());
+    } else {
+      dbClient = dbClientFuture.result();
+      future.complete();
+      LOGGER.debug("DB client creation completed.");
+    }
+    return future;
+  }
+
+  private Future<Void> initHttpServer() {
+//    Future<Void> future = Future.future();
+//
+//    vertx.createHttpServer()
+//      .requestHandler(req -> req.response().end("Hello Vert.x! with multi init steps"))
+//      .listen(8181);
+//
+//    future.complete();
+//    return future;
+    Future<Void> future = Future.future();
+    HttpServer server = vertx.createHttpServer();
+
+    Router router = buildRouter();
+
+    server
+      .requestHandler(router::accept)
+      .listen(8080, ar -> {
+        if (ar.succeeded()) {
+          LOGGER.info("HTTP server running on port 8080");
+          future.complete();
+        } else {
+          LOGGER.error("Could not start a HTTP server", ar.cause());
+          future.fail(ar.cause());
+        }
+      });
+    return future;
+  }
+
+  private Router buildRouter() {
+
+    Router router = Router.router(vertx);
+
+    router.get("/").handler(this::indexHandler);
+    router.get("/wiki/:page").handler(this::pageRenderingHandler);
+    router.post().handler(BodyHandler.create());
+    router.put("/save").handler(this::pageUpdateHandler);
+    router.post("/create").handler(this::pageCreateHandler);
+    router.delete("/delete").handler(this::pageDeletionHandler);
+
+    return router;
+  }
+
+  private void pageDeletionHandler(RoutingContext routingContext) {
+    routingContext.response().setStatusCode(200);
+    routingContext.response().end();
+  }
+
+  private void pageCreateHandler(RoutingContext routingContext) {
+    routingContext.response().setStatusCode(201);
+    routingContext.response().end();
+  }
+
+  private void pageUpdateHandler(RoutingContext routingContext) {
+
+    routingContext.response().end("<h2>pageUpdateHandler</h2>");
+  }
+
+  private void pageRenderingHandler(RoutingContext routingContext) {
+    String page = routingContext.request().getParam("page");
+    routingContext.response().setStatusCode(200);
+    routingContext.response().end("<h2>pageRenderingHandler ::" +
+      page +"</h2>");
+  }
+
+  private void indexHandler(RoutingContext routingContext) {
+    routingContext.response().setStatusCode(200);
+    routingContext.response().end("<h2>indexHandler</h2>");
+  }
+}
